d16c6ea2d57c194bea64777b3cc6df4259986d35
ibtesam5d <ibtesam.hossain@gmail.com>
2023-04-25 14:15:35 -0600

    CreatePost Template

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb=2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900 py-3"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="py-1 px-2 bg-[#ececf1] text-black font-semibold rounded-[5px] text-xs"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
+    </div>
+  );
 };
 
 export default FormField;
