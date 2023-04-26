cd2806cc1964c988dbff63444bcf912e86eba562
ibtesam5d <ibtesam.hossain@gmail.com>
2023-04-25 18:06:25 -0600

    creating a post

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -39,7 +39,32 @@ const CreatePost = () => {
   };
   console.log(form);
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        navigate("/");
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("please enter a prompt and generate image first to share");
+    }
+  };
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
